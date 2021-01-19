bb8e9b28ff8e02c56361c9ee687a77fedf4481d8
Josue Majano <josue_majano@yahoo.com>
2021-01-19 15:40:33 -0600

    second commit

--- src/components/Lifecycle.js
+++ src/components/Lifecycle.js
@@ -21,4 +21,24 @@ class Lifeycle extends React.Component {
     componentDidUpdate() {
         console.log('componentDidUpdate()');
     }
+
+    render() {
+        console.log('render()');
+        return (
+                <>
+                <h1>Lifecycle Demo {this.state.count}</h1>
+                <button onClick={this._increment}>+</button>
+                <h2>App Count: {this.props.appCount} </h2>
+                </>
+        );
+    }
+
+    _increment = () => {
+        console.log('_increment()');
+        this.setState({
+            count: this.state.count + 1
+        }, () => {
+            console.log('this.setState() finished')
+        })
+    }
 }
